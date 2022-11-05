cdaeb13b3ee1afefbda6ed4dc7204dcae7589a3b
pengrey <rodrigoflima.me@gmail.com>
2022-11-05 12:37:18 +0000

    Adde prompt and menu schafold

--- C2-Server/main.go
+++ C2-Server/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"log"
+	"os"
 
 	p2p "github.com/leprosus/golang-p2p"
 )
@@ -16,7 +18,52 @@ type Buy struct {
 	Text string
 }
 
-func main() {
+func getArguments() (int, string, bool) {
+	// Get port to be used
+	PORT := flag.Int("port", 8080, "Port to be used for communication")
+
+	// Defining a string flag
+	PASSWD := flag.String("password", "", "Password to be used for authentication of the agents")
+
+	// Define if program is to be run on silent mode
+	SILENT := flag.Bool("silent", false, "Remove prompt from startup")
+
+	// Call flag.Parse() to parse the command-line flags
+	flag.Parse()
+
+	return *PORT, *PASSWD, *SILENT
+}
+
+func getPrompt() {
+	version := "1.0"
+	prompt := " (                          *     \n )\\ )         (           (  `    \n(()/(         )\\          )\\))(   \n /(_))     ((((_)(       ((_)()\\  \n(_))        )\\ _ )\\      (_()((_) \n| _ \\       (_)_\\(_)     |  \\/  | \n|   /   _    / _ \\    _  | |\\/| | \n|_|_\\  (_)  /_/ \\_\\  (_) |_|  |_| "
+
+	fmt.Println(prompt)
+	fmt.Printf("\n[Remote Anamnestic Mapper (v%s)]\n\n", version)
+}
+
+func menu() {
+	var inpt string
+	for true {
+		fmt.Println("[?] Choose one of the options:\n       1 - List Agents\n       2 - Request Agent's RAM\n       3 - Quit")
+		fmt.Print("> ")
+
+		fmt.Scanln(&inpt)
+
+		switch inpt {
+		case "1":
+			fmt.Println("Running ")
+		case "2":
+			fmt.Println("Running ")
+		case "3":
+			os.Exit(0)
+		default:
+			fmt.Printf("[!] Option %s doesnt exist!\n", inpt)
+		}
+	}
+}
+
+func serverStart() {
 	tcp := p2p.NewTCP("localhost", "8080")
 
 	server, err := p2p.NewServer(tcp)
@@ -45,4 +92,25 @@ func main() {
 	if err != nil {
 		log.Panicln(err)
 	}
+}
+
+func main() {
+	// Get arguments from user
+	port, password, silent := getArguments()
+
+	// Print Prompt
+	if !silent {
+		getPrompt()
+	}
+
+	// Check vars
+	fmt.Println("[*] Port being used: ", port)
+	if password != "" {
+		fmt.Println("[*] Password being used: ", password)
+	} else {
+		fmt.Println("[!] WARNING! The server is being run without a password!")
+	}
+
+	// Start Menu
+	menu()
 }
