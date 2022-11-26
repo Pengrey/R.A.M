8ee2608712f6381439d4f7d776b0357387d2603b
Inryatt <inrymail@gmail.com>
2022-11-26 20:29:08 +0000

    Fixed broken addr in main

--- server/main.go
+++ server/main.go
@@ -176,7 +176,7 @@ func requestRAM(port string) {
 
 			if inpt >= 0 && inpt < len(agentsList) {
 				fmt.Println("[+] Requesting RAM.")
-				sendMessage(msg, agentsList[inpt])
+				sendMessage(msg, strings.Split(agentsList[inpt], ":")[0]+":"+strings.Split(agentsList[inpt], ":")[1])
 				// Sleep for 1 second to give time to the server to be ready
 				time.Sleep(1 * time.Second)
 
